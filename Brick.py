9b1fffb3faabba2a10575bbc49c7d5becbe93b36
Zach Veenstra <zach.veen.gr@gmail.com>
2022-04-07 16:16:20 -0400

    Bricks now show up in a grid

--- Brick.py
+++ Brick.py
@@ -7,9 +7,10 @@ class Brick(pg.sprite.Sprite):
     color. The health of the brick is calculated by summing the values
     of each RGB value. Each time the brick is hit, 25 health is removed."""
 
-    def __init__(self):
+    def __init__(self, width, height, x, y):
         pg.sprite.Sprite.__init__(self)
-        self.__image = pg.Surface((200, 200))
+
+        self.__image = pg.Surface((width, height))
 
 
         self.__r = random.randint(0,255)
@@ -20,8 +21,8 @@ class Brick(pg.sprite.Sprite):
 
         self.__image.fill( (self.__r, self.__g, self.__b) )
         self.__rect = self.__image.get_rect()
-        self.__rect.x = 200
-        self.__rect.y = 200
+        self.__rect.x = x
+        self.__rect.y = y
 
     def draw(self, screen):
         screen.blit(self.__image, self.__rect)
--- Game.py
+++ Game.py
@@ -7,24 +7,59 @@ from Paddle import Paddle
 
 class Game():
     """Sets up the environment, all of the balls, bricks paddle, etc.,
-    and contains the game loop."""
+    and contains the game loop.
+    """
 
     def __init__ (self):
         """Constructor"""
         pg.init()
-        self.__screen = pg.display.set_mode((800,600))
+
+        # The screen width in height, measured by pixels.
+        self.__SCREEN_WIDTH = 800
+        self.__SCREEN_HEIGHT = 600
+
+        # The rows and columns of bricks.
+        self.__ROWS = 5
+        self.__COLUMNS = 8
+
+        # The width and height of bricks. The bricks should take up the 
+        # top half of the screen.
+        self.__BRICK_WIDTH = self.__SCREEN_WIDTH / self.__COLUMNS
+        self.__BRICK_HEIGHT = self.__SCREEN_HEIGHT / self.__ROWS / 2
+
+        # Sets the width and height of the screen
+        self.__screen = pg.display.set_mode((self.__SCREEN_WIDTH,
+                                             self.__SCREEN_HEIGHT))
+
         self.__clock = pg.time.Clock()
+
         self.__running = True
-        self.__boxie = Brick()
-        self.__overlay = Overlay()
-        self.__paddle = Paddle()
-        self.__overlay = Overlay()
-        self.__paddle = Paddle()
+
+        self.__bricks  = []
+
         self.__balls = []
+
+        self.__overlay = Overlay()
+
+        self.__paddle = Paddle()
+
+        self.__overlay = Overlay()
+
+        self.__paddle = Paddle()
+
+
 
 
 
     def run(self):   
+
+        self.addBall()
+
+        # Loops through the x and y variables each brick will occupy.
+        for x in range(0, int(self.__COLUMNS * self.__BRICK_WIDTH), int(self.__BRICK_WIDTH)):
+            for y in range(0, int(self.__ROWS * self.__BRICK_HEIGHT), int(self.__BRICK_HEIGHT)):
+                self.addBrick(self.__BRICK_WIDTH, self.__BRICK_HEIGHT, x, y)
+
         while self.__running:
 
             # Event handling.
@@ -32,17 +67,21 @@ class Game():
                 if event.type == pg.QUIT:
                     self.__running = False
 
-            # Object Updating.
-            self.__boxie.update()
+            
 
             # Redrawing.
             self.__screen.fill((255,255,255))
-            self.__boxie.draw(self.__screen)
-            self.__overlay.draw(self.__screen)
+            
             self.__paddle.draw(self.__screen)
-            self.addBall()
+            
+
+            for brick in self.__bricks:
+                brick.draw(self.__screen)
+
             for ball in self.__balls:
                 ball.draw(self.__screen)
+
+            self.__overlay.draw(self.__screen)
 
             pg.display.flip()
             self.__clock.tick(60)
@@ -51,6 +90,20 @@ class Game():
         pg.quit()
 
     def addBall(self):
-        """Adds a ball to the screen."""
+        """Adds a ball to the list of balls so that multiple
+        may appear on the screen.
+        """
+
         ball = Ball()
         self.__balls.append(ball)
+
+    def addBrick(self, width, height, x, y):
+        """Adds a brick to the list of bricks so that multiple may
+        appear on the screen.
+        """
+
+        brick = Brick(width, height, x, y)
+        self.__bricks.append(brick)
+
+    def getWidth(self):
+        return self.__SCREEN_WIDTH
